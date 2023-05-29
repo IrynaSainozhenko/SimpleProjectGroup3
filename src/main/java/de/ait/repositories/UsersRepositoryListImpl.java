def9d0320c2b58e388f123ded52786a588a7a43d
IrynaSainozhenko <zavatuhina@gmail.com>
2023-05-30 00:29:00 +0200

    add tests and change List<Uzer> uzers

--- src/main/java/de/ait/repositories/UsersRepositoryListImpl.java
+++ src/main/java/de/ait/repositories/UsersRepositoryListImpl.java
@@ -7,9 +7,9 @@ import java.util.List;
 
 public class UsersRepositoryListImpl implements UsersRepository {
     private List<User> users = new ArrayList<>(List.of(
-            new User("User1", "User1", 20, 1.81),
-            new User("User2", "User2", 25, 1.85),
-            new User("User3", "User3", 30, 1.90)
+            new User("Amina", "Zolotoverkh", 20, 1.71),
+            new User("Andrey", "Didenko", 25, 1.85),
+            new User("Dmytro", "Lukash", 30, 1.90)
     ));
     @Override
     public List<User> findAll() {
--- src/test/UsersServiceImplTest.java
+++ src/test/UsersServiceImplTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -15,16 +16,43 @@ class UsersServiceImplTest {
         this.usersService = new UsersServiceImpl(new UsersRepositoryListImpl());
     }
 
+    //    1. Вывести имена всех пользователей
     @Test
     void getNames() {
         List<String> actual = usersService.getNames();
-        List<String> expected = List.of("User1", "User2", "User3");
+        List<String> expected = List.of("Amina", "Andrey", "Dmytro");
         Assertions.assertEquals(expected, actual);
     }
+
+    //  2. Вывести фамилию самого взрослого пользователя
+    @Test
+    public void getLastNameOfMostAging() {
+        String actuals = usersService.getLastNameOfMostAging();
+        String expected = "Lukash";
+        assertEquals(expected, actuals);
+    }
+
+    //  4. Вывести средний возраст всех пользователей
+    @Test
+    public void getAverageAgeOfUsers() {
+        double actuals = usersService.getAverageAgeOfUsers();
+        double expected = (20 + 25 + 30) / 3;
+        assertEquals(expected, actuals);
+    }
+
+    //  5. Вывести возраст самого высокого человека
     @Test
     void getAgeOfTheHighest(){
         int actual = usersService.getAgeOfTheHighest();
         int expected = 30;
         assertEquals(expected,actual);
+    }
+
+//    6. Вывести имя и фамилию самого низкого человека
+    @Test
+    public void getShortestPersonFullName(){
+        String actuals = usersService.getShortestPersonFullName();
+        String expected = "Amina Zolotoverkh";
+        assertEquals(expected, actuals);
     }
 }
